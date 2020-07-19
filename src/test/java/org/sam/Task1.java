daa3f8378cf98a8c6e11b4f224bcae55855b417e
Divya241 <divyakrishnamoorthy24@gmail.com>
2020-07-19 12:57:48 +0530

    List added

--- src/test/java/org/sam/Task1.java
+++ src/test/java/org/sam/Task1.java
@@ -1,8 +1,12 @@
 package org.sam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
 	public static void main(String[] args) {
-		
+		List li=new ArrayList();
+		li.add(10);
+		li.add(20);
 	}
-
-}
+	}
