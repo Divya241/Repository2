7cd2e7b308e6f34ecfdfaf826fa64c7fb4949444
Divya241 <divyakrishnamoorthy24@gmail.com>
2020-07-21 12:18:57 +0530

    Set is added

--- src/test/java/org/sam/Task1.java
+++ src/test/java/org/sam/Task1.java
@@ -1,8 +1,11 @@
 package org.sam;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Task1 {
 	public static void main(String[] args) {
+		Set si=new LinkedHashSet();
 		
 	}
-
 }
